44a6316aa4e0e49c16db3f4bc2464ee8022a3d4e
mALAKa82973i <tricks_parsons0u@icloud.com>
2025-07-22 12:32:38 +0100

    fix this bug

--- 3Q0d2/ohRFv/rest_9lx7p_1747915680.log.ts
+++ 3Q0d2/ohRFv/rest_9lx7p_1747915680.log.ts
@@ -3,7 +3,6 @@ import { RuleConfigSeverity } from '@commitlint/types';
 const config: UserConfig = {
   rules: {
       RuleConfigSeverity.Error,
-      [
         'docs',
         'fix',
         'release',
